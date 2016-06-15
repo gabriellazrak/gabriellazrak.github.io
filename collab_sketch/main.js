ed8255e71036006d4c5c61500bf1dd6bce8cef5a
Gabriel <clonewars310@yahoo.com>
2016-06-15 17:43:42 +0000

    collab_sketch

--- collab_sketch/main.js
+++ collab_sketch/main.js
@@ -8,11 +8,12 @@ var config = {
 
 var pointsData=firebase.database().ref();
 var points=[];
-
+var r_g_b;
 function setup(){
     var canvas=createCanvas(window.innerWidth-50,window.innerHeight-50);
     background(255);
-    fill(0);
+    r_g_b=(0,0,0);
+    fill(r_g_b);
     pointsData.on("child_added",function(point){
         points.push(point.val());
     });
@@ -27,12 +28,14 @@ function draw(){
     background(255);
     for (var i = 0; i < points.length; i++) {
         var point = points[i];
+        fill(point.rgb);
+        stroke(point.rgb);
         ellipse(point.x, point.y, 5, 5);
     }
 }
 
 function drawPoint() {
-  pointsData.push({x: mouseX, y: mouseY});
+  pointsData.push({x: mouseX, y: mouseY, rgb: r_g_b});
 }
 $("#saveDrawing").on("click",saveDrawing);
 function saveDrawing(){
